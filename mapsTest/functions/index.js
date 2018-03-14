8b803a28105500578e29477de4005d4ba4213c77
Sean Martin <seankieran.m@hotmail.com>
2018-03-14 17:25:30 +0000

    Generating 20 locations works

--- mapsTest/functions/index.js
+++ mapsTest/functions/index.js
@@ -1,57 +1,64 @@
 const functions = require('firebase-functions');
-//Note that I changed this from a var to a const change back if causes problems
-
-  //Take in a location and a radius(?)
-//Generate one point inside of this circle - how to make it a diff point
-
-    //Get a random location inside of the starting circle, then
-    //Try to move this to a place
-    //If a new place can't be found, put it on a road
-    //We need to make sure these generated points still lie inside the game area
-
-//For a number of points repeat the above
 const randomLocation = require('random-location')
 
 //NOTE the arrow operator => creates arrow functions, very similar to lambda
 //functions in other languages
 firestore_location = '/locations/{documentId}'
-//Perhaps this should be on on write
-// exports.TowerLocation = functions.firestore.document(firestore_location).onCreate((event) => {
+
+//Note, perhaps this should be on on write
+exports.PlaceLocations = 
+functions.firestore.document(firestore_location).onCreate(
+    (event) => {
         
-    var googleMapsClient = require('@google/maps').createClient({
-        key: "GET_YOUR_OWN",
-        Promise: Promise
-    });
-    
-    //This will be set when we call this function from firebase
-    var centre = {
-        latitude: 37.7768006,
-        longitude: -122.4187928
-      }
+        var googleMapsClient = require('@google/maps').createClient({
+            key: "GET_YOUR_OWN",
+            Promise: Promise
+        });
+        
+        //This will be set when we call this function from firebase
+        var centre = {
+            latitude: 37.7768006,
+            longitude: -122.4187928
+        }
 
-    var search_radius = 500
-    
-    //TODO it could be a good idea to put a timeout in this
-    //Not sure what the unit for this timeout is though
-    var places_request = {
-        language: 'en',
-        location: centre,
-        radius: search_radius,
-        rankby: 'prominence',
-    }
+        var search_radius = 500
+        var num_points_required = 20
+        
+        //TODO it could be a good idea to put a timeout in this
+        //Not sure what the unit for this timeout is though
+        var places_request = {
+            language: 'en',
+            location: centre,
+            radius: search_radius,
+            rankby: 'prominence',
+        }
 
-    //Return later
-    googleMapsClient.placesNearby(places_request)
-    .asPromise()
-    .then(places => {
-        console.log(places.json.results.length)
-        //console.log(JSON.stringify(places, null, 2))
+        return googleMapsClient.placesNearby(places_request)
+        .asPromise()
+        .then(response => {
+            var places = response.json.results
+            console.log("Using ahhhahahh")
+            console.log(JSON.stringify(places, null, 2))
+            
+            var generated_locations = {"locations": []}
+            for(i = 0; i < places.length; ++i) {
+                var location = places[i].geometry.location
+                generated_locations.locations.push(location)
+            }
+
+            //If not enough points, return some random ones
+            for(i = 0; i < num_points_required - places.length; ++i) {
+                var randomPoint = 
+                randomLocation.randomCirclePoint(centre, search_radius)
+                generated_locations.locations.push(randomPoint)
+            }
+
+            return event.data.ref.set({generated_locations}, {merge: true})
+        }
+        )
+        .catch(err => {
+            console.log(err)
+        }
+        )
     }
-    )
-    .catch(err => {
-        console.log(err)
-    }
-    )
-    //If not enough points, return some random ones
-    //Use the below to do that
-    //const randomPoint = randomLocation.randomCirclePoint(P, R)
+)
