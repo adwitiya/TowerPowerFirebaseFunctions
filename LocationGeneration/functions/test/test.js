730525ecf1f7a63f19c255fe6ac4b0e726bd9340
Balakumaran Palanivel <kumaraxe@gmail.com>
2018-03-29 18:16:38 +0100

    [master] adding unit test of find farthest location

--- LocationGeneration/functions/test/test.js
+++ LocationGeneration/functions/test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-const expect = require('chai').expect
+const chai = require('chai')
 const placeLocations = require('../index_UT');
 
 describe('Return Promise', function() {
@@ -36,5 +36,31 @@ describe('Test Remove Element', function(){
 
   it('Should Remove an Element', function(){
     assert.equal(input[0].latitude, output[0].latitude);
+  });
+});
+
+
+describe('Test Farthest Location', function(){
+  
+  var input = [
+    {
+      'latitude': 53.44564324,
+      'longitude':  -5.3435335
+    },
+    {
+      'latitude': 15.123123,
+      'longitude': 26.213123
+    } 
+  ];
+
+  var centre = {
+    'latitude' : 53.44566,
+    'longitude': -6.3435335
+  };
+
+  var return_val = placeLocations.FindNFarthestLocation(input, centre);
+
+  it('Should Return one of', function(){
+    chai.assert.oneOf(return_val.latitude, [input[0].latitude, input[1].latitude]);
   });
 });
